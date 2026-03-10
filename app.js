579531d4bace2491eefd55f8c6a37181a7e391e4
aviv lidar <lidar3456@gmail.com>
2026-03-10 20:01:05 +0200

    Fix API URL for remote access via Ngrok

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿const API_URL = "api.php";
+﻿const API_URL = window.location.origin + window.location.pathname.replace(/\/[^\/]*$/, "") + "/api.php";
 
 // פונקציית עזר לעיצוב תאריך נוכחי עבור אינפוט מסוג datetime-local
 function getCurrentDateTimeLocal() {
@@ -120,3 +120,4 @@ async function sendPostRequest(action, data) {
 
 // הפעלת טעינת הדשבורד עם עליית העמוד
 document.addEventListener("DOMContentLoaded", loadDashboard);
+
