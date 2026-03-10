97a81e0a8ea7f0299fc95690212d61ed100a0a29
aviv lidar <lidar3456@gmail.com>
2026-03-10 20:05:17 +0200

    Allow ngrok-skip-browser-warning header in CORS for API requests

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, ngrok-skip-browser-warning");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     http_response_code(200);
@@ -87,3 +87,4 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
 
 $conn->close();
 ?>
+
